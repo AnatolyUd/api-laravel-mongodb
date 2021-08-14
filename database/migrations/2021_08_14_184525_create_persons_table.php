4f51ba47f9e3ba5ea60b17c67cae892d6afaef8c
Anatoly Udod <anatolyud@gmail.com>
2021-08-14 22:56:21 +0300

    add laravel mongodb library, model Person and migration

---
+++ database/migrations/2021_08_14_184525_create_persons_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('persons', function (Blueprint $table) {
+            $table->id();
+            $table->string('login');
+            $table->string('email');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('age')->nullable();
+            $table->string('gender')->nullable();
+            $table->string('mobile_number')->nullable();
+            $table->string('city')->nullable();
+            $table->string('car_model')->nullable();
+            $table->string('salary')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('persons');
+    }
+}
